ce6293a853adb540805c0d4395e31039161ab357
颜海镜 <yanxuefeng@meituan.com>
2018-01-24 14:34:44 +0800

    feat: add tongji

---
+++ assets/plugin/tongji/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?53ee8889aa158cbb581bdbc9a373b5f0";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
